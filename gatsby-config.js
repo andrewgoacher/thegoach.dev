d568668b07ca7e9e0e5f3e2aa449413b68bd5690
Andrew Goacher <andrewgoacher@outlook.com>
2019-03-10 15:58:07 +0000

    Basic changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Andrew Goacher`,
+    description: `Kind of a blog I guess`,
+    author: `Andrew Goacher`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
