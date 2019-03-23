3e86e4e1a071da3cd437784946adfccdd78eff19
Andrew Goacher <andrewgoacher@outlook.com>
2019-03-23 16:56:00 +0000

    Remove blog posts for now

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,63 +7,69 @@ import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 
 class BlogIndex extends React.Component {
+  getPosts(posts) {
+    if(!posts) {
+      return null;
+    }
+    return posts.map(({ node}) => {
+      const title = node.frontmatter.title || node.fields.slug;
+      return (
+        <div key={node.fields.slug}>
+          <h3
+            style={{
+              marginBottom: rhythm(1/4)
+            }}>
+           <link style={{boxShadow: 'none'}} to={node.fields.slug}>
+             {title}</link>
+          </h3>
+          <small>{node.frontmatter.date}</small>
+          <p dangerouslySetInnerHTML={{__html: node.frontmatter.description || node.excerpt}} />
+        </div>
+      );
+    });
+  }
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
-    const posts = data.allMarkdownRemark.edges
+    // let posts =  null;
+    // if(data.allMarkdownRemark && data.allMarkdownRemark.edges) {
+    //   posts = this.getPosts(data.allMarkdownRemark.edges);
+    // }
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return (
-            <div key={node.fields.slug}>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </div>
-          )
-        })}
+        <p>
+          There are currently no posts to see here!
+        </p>
       </Layout>
     )
   }
 }
 
 export default BlogIndex
-
+//
 export const pageQuery = graphql`
   query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
+      site {
+          siteMetadata {
+              title
           }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            description
-          }
-        }
       }
-    }
-  }
-`
+  }`
+//    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+//       edges {
+//         node {
+//           excerpt
+//           fields {
+//             slug
+//           }
+//           frontmatter {
+//             date(formatString: "MMMM DD, YYYY")
+//             title
+//             description
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
