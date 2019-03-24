dc682f52af3f2a9daa365261cbe420280fad3d23
Andrew Goacher <andrewgoacher@outlook.com>
2019-03-24 22:07:48 +0000

    Adding custom html file.  Layout coming along fine

--- src/components/layouts/pageLayout.js
+++ src/components/layouts/pageLayout.js
@@ -1,58 +1,44 @@
-import React from 'react';
-import './pageLayout.css';
-import {Link} from 'gatsby';
-import Menu from '../menu';
-import Avatar from '../avatar';
+import React from "react"
+import "./pageLayout.css"
+import { Link } from "gatsby"
+import Menu from "../menu"
+import Avatar from "../avatar"
 
 class PageLayout extends React.Component {
   render() {
-    const {title, children} = this.props;
+    const { title, children } = this.props
 
     return (
       <div className="layout">
-        <div className="upper">
-          <header>
-
-          </header>
-          <nav>
-
-          </nav>
-        </div>
+        <div className="upper"></div>
+        <header>
+          <h1>
+            <Link to={`/`}>
+              {title}
+            </Link>
+          </h1>
+        </header>
+        <nav>
+          <Menu/>
+        </nav>
         <main>
           {children}
         </main>
-        <div className="lower">
-          <footer>
-
-          </footer>
-          <div className="social">
-          </div>
+        <div className="lower"></div>
+        <footer>
+          © Andrew Goacher {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+        <div className="social">
+          <i className="fa-c fab fa-github fa-3x"></i>
+          <i className="fab fa-gitlab fa-c fa-3x"></i>
+          <i className="fab fa-bitbucket fa-c fa-3x"></i>
+          <i className="fab fa-twitter fa-c fa-3x"></i>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default PageLayout;
-
-{/*<div className="grid">*/}
-  {/*<header>*/}
-    {/*<h1>*/}
-      {/*<Avatar />*/}
-      {/*<Link to={`/`}>*/}
-        {/*{title}*/}
-      {/*</Link>*/}
-    {/*</h1>*/}
-  {/*</header>*/}
-  {/*<nav>*/}
-    {/*<Menu />*/}
-  {/*</nav>*/}
-  {/*<main>*/}
-    {/*{children}*/}
-  {/*</main>*/}
-  {/*<footer>*/}
-    {/*© Andrew Goacher {new Date().getFullYear()}, Built with*/}
-    {/*{` `}*/}
-    {/*<a href="https://www.gatsbyjs.org">Gatsby</a>*/}
-  {/*</footer>*/}
-{/*</div>*/}
+export default PageLayout
