266f133889ad58d87a4a36ab8b8aa8adb764b34d
Andrew Goacher <andrewgoacher@outlook.com>
2019-03-27 00:50:41 +0000

    Ad basic about page info and add some initial images of the boys

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,56 +1,85 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { Link, graphql, StaticQuery } from "gatsby"
+import Image from 'gatsby-image';
 
 import SEO from "../components/seo"
-import PageLayout from '../components/layouts/pageLayout';
+import PageLayout from "../components/layouts/pageLayout"
 
-// import { rhythm } from "../utils/typography"
+const About = ({ location }) => {
+  return (
+    <StaticQuery query={imageQueries} render={data => {
+      const author = data.site.siteMetadata
+      return (
+        <PageLayout location={location}>
+          <SEO
+            title="About me"
+          />
+          <h1>Hi I'm Andrew!</h1>
 
-class About extends React.Component {
-  render() {
-    // const { data } = this.props
-    // const siteTitle = data.site.siteMetadata.title
+          <p>
+            I'm a 'full stack' developer working in Manchester UK. I mostly work with C# and .NET backends and react or
+            angular front ends.</p>
+          <p>
+            I'm easy on technology choice.
+            In my own time I'm looking at rust and F# and tinkering with side projects whenever the mood strikes.
+          </p>
 
-    return (
-      <PageLayout location={this.props.location}>
-        <SEO
-          title="About me"
-        />
-        <h1>About page!</h1>
+          <p>
+            I don't get a lot done because of these 3 idiots.
+          </p>
+            <Image
+              fixed={data.charlie.childImageSharp.fixed}
+              alt={author}
+              style={{ borderRadius: "100%" }}
+              imgStyle={{ borderRadius: "50%" }}/>
 
-        <p>
-          Some content should be here
-        </p>
-      </PageLayout>
-    )
-  }
+            <Image
+              fixed={data.bigfoot.childImageSharp.fixed}
+              alt={author}
+              style={{ borderRadius: "100%" }}
+              imgStyle={{ borderRadius: "50%" }}/>
+
+
+            <Image
+              fixed={data.merlin.childImageSharp.fixed}
+              alt={author}
+              style={{ borderRadius: "100%" }}
+              imgStyle={{ borderRadius: "50%" }}/>
+
+        </PageLayout>
+      )
+    }}/>
+  )
 }
 
 export default About
-//
-// export const pageQuery = graphql`
-//     query {
-//         site {
-//             siteMetadata {
-//                 title
-//             }
-//         }
-//     }
-// `
-/*
-#    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-#      edges {
-#        node {
-#          excerpt
-#          fields {
-#            slug
-#          }
-#          frontmatter {
-#            date(formatString: "MMMM DD, YYYY")
-#            title
-#            description
-#          }
-#        }
-#      }
-#    }
- */
+const imageQueries = graphql`
+    query ImageQueries {
+        site {
+            siteMetadata {
+                author
+            }
+        }
+        charlie: file(absolutePath: {regex: "/charlie.jpg/"}) {
+            childImageSharp {
+                fixed(width: 100, height: 100) {
+                    ...GatsbyImageSharpFixed
+                }
+            }
+        }
+        merlin: file(absolutePath: {regex: "/merlin.jpg/"}) {
+            childImageSharp {
+                fixed(width: 100, height: 100) {
+                    ...GatsbyImageSharpFixed
+                }
+            }
+        }
+        bigfoot: file(absolutePath: {regex: "/bigfoot.jpg/"}) {
+            childImageSharp {
+                fixed(width: 100, height: 100) {
+                    ...GatsbyImageSharpFixed
+                }
+            }
+        }
+    }
+`
