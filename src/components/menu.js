f12c9904ebb0bd79a544d265a8bdf3476d9b0f59
Andrew Goacher <andrewgoacher@outlook.com>
2019-03-29 14:54:57 +0000

    Tidied menu component

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,32 +1,31 @@
 import React from "react"
-
 import { Link } from "gatsby"
 
 class Menu extends React.Component {
   createLink(name, path, location) {
-    let className = "";
-    let loc = location.pathname;
+    let className = ""
+    let loc = location.pathname
 
-    if(loc.length > 1 && loc.lastIndexOf('/') === loc.length -1 ) {
-      loc = loc.substring(0, loc.length-1);
+    if (loc.length > 1 && loc.lastIndexOf("/") === loc.length - 1) {
+      loc = loc.substring(0, loc.length - 1)
     }
 
-    if(loc === path) {
-      className = "selected";
+    if (loc === path) {
+      className = "selected"
     }
 
     return (
       <Link to={path} className={className}>
         {name}
       </Link>
-    );
+    )
   }
 
   render() {
     const { location } = this.props
     return (
       <ul>
-         <li>
+        <li>
           {this.createLink("Home", `/`, location)}
         </li>
         <li>
