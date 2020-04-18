ff8d28389ed77f275ce1547b1b86b23192ed1e4a
Andrew Goacher <andrewgoacher@outlook.com>
2020-04-19 00:52:31 +0100

    Lets see if this works to stop hanging CI

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,12 @@
 ssh -o StrictHostKeyChecking=no $SSH_USER@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
   cd /app
   export $(cat .env | xargs)
-  docker-compose down
   pkill thegoach_dev
   cd ..
   rm -rf /app
   mv /app_new /app
   mkdir /app_new
   cd /app
-  ./thegoach_dev &
   docker-compose up -d
+  ./thegoach_dev & disown
 ENDSSH
