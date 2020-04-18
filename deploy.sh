e4c2dcede9bca0e1bf36bf1465ac37dc97a17254
Andrew Goacher <andrewgoacher@outlook.com>
2020-04-18 22:17:30 +0100

    Change var name and kill docker compose before recreating

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ ssh -o StrictHostKeyChecking=no $SSH_USER@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
   cd /app
   export $(cat .env | xargs)
   docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
-  docker pull $IMAGE:web
-  docker-compose -f docker-compose.yml up -d
+  docker pull $IMAGE
+  docker-compose down
+  docker-compose up -d
 ENDSSH
