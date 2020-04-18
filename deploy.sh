73a301fb137cc203cc78c6c1e69457ee7abe82fd
Andrew Goacher <andrewgoacher@outlook.com>
2020-04-19 00:33:28 +0100

    Remove docker pull from deploy

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,6 @@
 ssh -o StrictHostKeyChecking=no $SSH_USER@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
   cd /app
   export $(cat .env | xargs)
-  docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
-  docker pull $IMAGE
   docker-compose down
   pkill thegoach_dev
   cd ..
