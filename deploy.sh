aeb4c3aaade7077596b9ca65f0c9a6af0512aa48
Andrew Goacher <andrewgoacher@outlook.com>
2020-05-22 09:16:39 +0100

    Moving back to docker to try and keeo this shit alive

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 ssh -o StrictHostKeyChecking=no $SSH_USER@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
   cd /app
   export $(cat .env | xargs)
-  pkill thegoach_dev
-  cd ..
-  rm -rf /app
-  mv /app_new /app
-  mkdir /app_new
-  cd /app
-  docker-compose up -d
-  nohup ./thegoach_dev > waste.out 2> waste.err < /dev/null &
+  docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
+  docker pull $IMAGE:web
+  docker-compose -f docker-compose.prod.yml up -d
 ENDSSH
