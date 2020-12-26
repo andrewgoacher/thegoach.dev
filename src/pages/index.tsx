f556882b6b484732e1bfde6f4d25e85cf14c3aa9
Andrew Goacher <goach@hey.com>
2020-12-26 17:13:10 +0000

    Copy changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,10 +9,18 @@ const Index = () => {
       <div className='row section topspace'>
         <div className='col-md-12'>
           <p className='lead text-center text-muted'>
-            One day, there will be about 6 blog posts on here and I'll consider
-            myself a successful <a href='/blog'>blogger</a>.<br />
-            I'm a programmer, you can find out more about me{' '}
-            <a href='/about'>here</a>.
+            Hi I'm Andrew and I'm a full stack (ish) developer.
+          </p>
+          <p>
+            I've been a hilariously blogless blogger for a while now and I've
+            decided it's time to be honest to myself and give up on that
+            endeavour. Like a lot of developers, I still like building small
+            things or tinkering at home. I intend to find things of substance to
+            talk about and add them here as and when I am comfortable doing so.
+          </p>
+          <p>
+            My repositories and LinkedIn profile are below and I'm fairly
+            responsive on social platforms.
           </p>
         </div>
       </div>
